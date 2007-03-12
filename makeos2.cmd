a3a08467014fe27aeef984edb5db872ef811fa26
Ryan C. Gordon <icculus@icculus.org>
2007-03-12 01:01:58 +0000

    First shot at updating makeos2.cmd for Innotek/kLIBC's build tools. Farewell,  EMX, you have served me well. Thanks to Dave Yeo for the details on this.
    (This is untested at the moment.)

--- makeos2.cmd
+++ makeos2.cmd
@@ -1,14 +1,16 @@
 @echo off
 rem this is a simple batch file to build PhysicsFS on OS/2. You need to have
-rem  the EMX development tools installed for this to work.
+rem  the Innotek libc and GCC (or "kLIBC") installed for this to work:
+rem
+rem     http://svn.netlabs.org/libc
 rem
 rem This script (and, indeed, our OS/2 support) could use some tweaking.
 rem  Patches go to icculus@icculus.org ...
 
 set PHYSFSLANG=PHYSFS_LANG_ENGLISH
 set DEBUGFLAGS=-D_NDEBUG -O2 -s
-rem set CFLAGS=%DEBUGFLAGS% -Wall -Werror -Zomf -Zmt -Zmtd -I. -Izlib123 -c -D__ST_MT_ERRNO__ -DOS2 -DZ_PREFIX -DPHYSFS_SUPPORTS_ZIP -DPHYSFS_SUPPORTS_GRP -DPHYSFS_SUPPORTS_WAD -DPHYSFS_SUPPORTS_QPAK -DPHYSFS_SUPPORTS_HOG -DPHYSFS_SUPPORTS_MVL -DPHYSFS_LANG=%PHYSFSLANG% -DHAVE_ASSERT_H
-set CFLAGS=%DEBUGFLAGS% -Wall -Werror -Zomf -Zmt -Zmtd -I. -Izlib123 -c -D__ST_MT_ERRNO__ -DOS2 -DZ_PREFIX -DPHYSFS_SUPPORTS_ZIP -DPHYSFS_SUPPORTS_GRP -DPHYSFS_SUPPORTS_WAD -DPHYSFS_SUPPORTS_QPAK -DPHYSFS_SUPPORTS_HOG -DPHYSFS_SUPPORTS_MVL -DHAVE_ASSERT_H
+rem set CFLAGS=%DEBUGFLAGS% -Wall -Werror -Zomf -Zmt -Zmtd -I. -Izlib123 -c -D__ST_MT_ERRNO__ -DOS2 -DZ_PREFIX -DPHYSFS_SUPPORTS_ZIP -DPHYSFS_SUPPORTS_7Z -DPHYSFS_SUPPORTS_GRP -DPHYSFS_SUPPORTS_WAD -DPHYSFS_SUPPORTS_QPAK -DPHYSFS_SUPPORTS_HOG -DPHYSFS_SUPPORTS_MVL -DPHYSFS_LANG=%PHYSFSLANG% -DHAVE_ASSERT_H
+set CFLAGS=%DEBUGFLAGS% -Wall -Werror -Zomf -I. -Iz -c -D__ST_MT_ERRNO__ -DOS2 -DZ_PREFIX -DPHYSFS_SUPPORTS_ZIP -DPHYSFS_SUPPORTS_7Z -DPHYSFS_SUPPORTS_GRP -DPHYSFS_SUPPORTS_WAD -DPHYSFS_SUPPORTS_QPAK -DPHYSFS_SUPPORTS_HOG -DPHYSFS_SUPPORTS_MVL -DHAVE_ASSERT_H
 
 rem goto :dolinking
 
@@ -31,93 +33,94 @@ rem goto :dolinking
 @echo DATA LOADONCALL NONSHARED MULTIPLE >> bin\physfs.def
 @echo DESCRIPTION 'PhysicsFS: http://icculus.org/physfs/' >> bin\physfs.def
 @echo EXPORTS >> bin\physfs.def
-@echo  "PHYSFS_getLinkedVersion" >> bin\physfs.def
-@echo  "PHYSFS_init" >> bin\physfs.def
-@echo  "PHYSFS_deinit" >> bin\physfs.def
-@echo  "PHYSFS_supportedArchiveTypes" >> bin\physfs.def
-@echo  "PHYSFS_freeList" >> bin\physfs.def
-@echo  "PHYSFS_getLastError" >> bin\physfs.def
-@echo  "PHYSFS_getDirSeparator" >> bin\physfs.def
-@echo  "PHYSFS_permitSymbolicLinks" >> bin\physfs.def
-@echo  "PHYSFS_getCdRomDirs" >> bin\physfs.def
-@echo  "PHYSFS_getBaseDir" >> bin\physfs.def
-@echo  "PHYSFS_getUserDir" >> bin\physfs.def
-@echo  "PHYSFS_getWriteDir" >> bin\physfs.def
-@echo  "PHYSFS_setWriteDir" >> bin\physfs.def
-@echo  "PHYSFS_addToSearchPath" >> bin\physfs.def
-@echo  "PHYSFS_removeFromSearchPath" >> bin\physfs.def
-@echo  "PHYSFS_getSearchPath" >> bin\physfs.def
-@echo  "PHYSFS_setSaneConfig" >> bin\physfs.def
-@echo  "PHYSFS_mkdir" >> bin\physfs.def
-@echo  "PHYSFS_delete" >> bin\physfs.def
-@echo  "PHYSFS_getRealDir" >> bin\physfs.def
-@echo  "PHYSFS_enumerateFiles" >> bin\physfs.def
-@echo  "PHYSFS_exists" >> bin\physfs.def
-@echo  "PHYSFS_isDirectory" >> bin\physfs.def
-@echo  "PHYSFS_isSymbolicLink" >> bin\physfs.def
-@echo  "PHYSFS_openWrite" >> bin\physfs.def
-@echo  "PHYSFS_openAppend" >> bin\physfs.def
-@echo  "PHYSFS_openRead" >> bin\physfs.def
-@echo  "PHYSFS_close" >> bin\physfs.def
-@echo  "PHYSFS_read" >> bin\physfs.def
-@echo  "PHYSFS_write" >> bin\physfs.def
-@echo  "PHYSFS_eof" >> bin\physfs.def
-@echo  "PHYSFS_tell" >> bin\physfs.def
-@echo  "PHYSFS_seek" >> bin\physfs.def
-@echo  "PHYSFS_fileLength" >> bin\physfs.def
-@echo  "PHYSFS_swapSLE16" >> bin\physfs.def
-@echo  "PHYSFS_swapULE16" >> bin\physfs.def
-@echo  "PHYSFS_swapSLE32" >> bin\physfs.def
-@echo  "PHYSFS_swapULE32" >> bin\physfs.def
-@echo  "PHYSFS_swapSLE64" >> bin\physfs.def
-@echo  "PHYSFS_swapULE64" >> bin\physfs.def
-@echo  "PHYSFS_swapSBE16" >> bin\physfs.def
-@echo  "PHYSFS_swapUBE16" >> bin\physfs.def
-@echo  "PHYSFS_swapSBE32" >> bin\physfs.def
-@echo  "PHYSFS_swapUBE32" >> bin\physfs.def
-@echo  "PHYSFS_swapSBE64" >> bin\physfs.def
-@echo  "PHYSFS_swapUBE64" >> bin\physfs.def
-@echo  "PHYSFS_getLastModTime" >> bin\physfs.def
-@echo  "PHYSFS_readSLE16" >> bin\physfs.def
-@echo  "PHYSFS_readULE16" >> bin\physfs.def
-@echo  "PHYSFS_readSLE32" >> bin\physfs.def
-@echo  "PHYSFS_readULE32" >> bin\physfs.def
-@echo  "PHYSFS_readSLE64" >> bin\physfs.def
-@echo  "PHYSFS_readULE64" >> bin\physfs.def
-@echo  "PHYSFS_readSBE16" >> bin\physfs.def
-@echo  "PHYSFS_readUBE16" >> bin\physfs.def
-@echo  "PHYSFS_readSBE32" >> bin\physfs.def
-@echo  "PHYSFS_readUBE32" >> bin\physfs.def
-@echo  "PHYSFS_readSBE64" >> bin\physfs.def
-@echo  "PHYSFS_readUBE64" >> bin\physfs.def
-@echo  "PHYSFS_writeSLE16" >> bin\physfs.def
-@echo  "PHYSFS_writeULE16" >> bin\physfs.def
-@echo  "PHYSFS_writeSLE32" >> bin\physfs.def
-@echo  "PHYSFS_writeULE32" >> bin\physfs.def
-@echo  "PHYSFS_writeSLE64" >> bin\physfs.def
-@echo  "PHYSFS_writeULE64" >> bin\physfs.def
-@echo  "PHYSFS_writeSBE16" >> bin\physfs.def
-@echo  "PHYSFS_writeUBE16" >> bin\physfs.def
-@echo  "PHYSFS_writeSBE32" >> bin\physfs.def
-@echo  "PHYSFS_writeUBE32" >> bin\physfs.def
-@echo  "PHYSFS_writeSBE64" >> bin\physfs.def
-@echo  "PHYSFS_writeUBE64" >> bin\physfs.def
-@echo  "PHYSFS_setBuffer" >> bin\physfs.def
-@echo  "PHYSFS_flush" >> bin\physfs.def
-@echo  "PHYSFS_mount" >> bin\physfs.def
-@echo  "PHYSFS_getMountPoint" >> bin\physfs.def
-@echo  "PHYSFS_setAllocator" >> bin\physfs.def
-@echo  "PHYSFS_getCdRomDirsCallback" >> bin\physfs.def
-@echo  "PHYSFS_getSearchPathCallback" >> bin\physfs.def
-@echo  "PHYSFS_enumerateFilesCallback" >> bin\physfs.def
-@echo  "PHYSFS_utf8toucs2" >> bin\physfs.def
-@echo  "PHYSFS_utf8fromucs2" >> bin\physfs.def
-@echo  "PHYSFS_utf8toucs4" >> bin\physfs.def
-@echo  "PHYSFS_utf8fromucs4" >> bin\physfs.def
-@echo  "PHYSFS_utf8fromlatin1" >> bin\physfs.def
+@echo "_PHYSFS_getLinkedVersion" >> bin\physfs.def
+@echo "_PHYSFS_init" >> bin\physfs.def
+@echo "_PHYSFS_deinit" >> bin\physfs.def
+@echo "_PHYSFS_supportedArchiveTypes" >> bin\physfs.def
+@echo "_PHYSFS_freeList" >> bin\physfs.def
+@echo "_PHYSFS_getLastError" >> bin\physfs.def
+@echo "_PHYSFS_getDirSeparator" >> bin\physfs.def
+@echo "_PHYSFS_permitSymbolicLinks" >> bin\physfs.def
+@echo "_PHYSFS_getCdRomDirs" >> bin\physfs.def
+@echo "_PHYSFS_getBaseDir" >> bin\physfs.def
+@echo "_PHYSFS_getUserDir" >> bin\physfs.def
+@echo "_PHYSFS_getWriteDir" >> bin\physfs.def
+@echo "_PHYSFS_setWriteDir" >> bin\physfs.def
+@echo "_PHYSFS_addToSearchPath" >> bin\physfs.def
+@echo "_PHYSFS_removeFromSearchPath" >> bin\physfs.def
+@echo "_PHYSFS_getSearchPath" >> bin\physfs.def
+@echo "_PHYSFS_setSaneConfig" >> bin\physfs.def
+@echo "_PHYSFS_mkdir" >> bin\physfs.def
+@echo "_PHYSFS_delete" >> bin\physfs.def
+@echo "_PHYSFS_getRealDir" >> bin\physfs.def
+@echo "_PHYSFS_enumerateFiles" >> bin\physfs.def
+@echo "_PHYSFS_exists" >> bin\physfs.def
+@echo "_PHYSFS_isDirectory" >> bin\physfs.def
+@echo "_PHYSFS_isSymbolicLink" >> bin\physfs.def
+@echo "_PHYSFS_openWrite" >> bin\physfs.def
+@echo "_PHYSFS_openAppend" >> bin\physfs.def
+@echo "_PHYSFS_openRead" >> bin\physfs.def
+@echo "_PHYSFS_close" >> bin\physfs.def
+@echo "_PHYSFS_read" >> bin\physfs.def
+@echo "_PHYSFS_write" >> bin\physfs.def
+@echo "_PHYSFS_eof" >> bin\physfs.def
+@echo "_PHYSFS_tell" >> bin\physfs.def
+@echo "_PHYSFS_seek" >> bin\physfs.def
+@echo "_PHYSFS_fileLength" >> bin\physfs.def
+@echo "_PHYSFS_swapSLE16" >> bin\physfs.def
+@echo "_PHYSFS_swapULE16" >> bin\physfs.def
+@echo "_PHYSFS_swapSLE32" >> bin\physfs.def
+@echo "_PHYSFS_swapULE32" >> bin\physfs.def
+@echo "_PHYSFS_swapSLE64" >> bin\physfs.def
+@echo "_PHYSFS_swapULE64" >> bin\physfs.def
+@echo "_PHYSFS_swapSBE16" >> bin\physfs.def
+@echo "_PHYSFS_swapUBE16" >> bin\physfs.def
+@echo "_PHYSFS_swapSBE32" >> bin\physfs.def
+@echo "_PHYSFS_swapUBE32" >> bin\physfs.def
+@echo "_PHYSFS_swapSBE64" >> bin\physfs.def
+@echo "_PHYSFS_swapUBE64" >> bin\physfs.def
+@echo "_PHYSFS_getLastModTime" >> bin\physfs.def
+@echo "_PHYSFS_readSLE16" >> bin\physfs.def
+@echo "_PHYSFS_readULE16" >> bin\physfs.def
+@echo "_PHYSFS_readSLE32" >> bin\physfs.def
+@echo "_PHYSFS_readULE32" >> bin\physfs.def
+@echo "_PHYSFS_readSLE64" >> bin\physfs.def
+@echo "_PHYSFS_readULE64" >> bin\physfs.def
+@echo "_PHYSFS_readSBE16" >> bin\physfs.def
+@echo "_PHYSFS_readUBE16" >> bin\physfs.def
+@echo "_PHYSFS_readSBE32" >> bin\physfs.def
+@echo "_PHYSFS_readUBE32" >> bin\physfs.def
+@echo "_PHYSFS_readSBE64" >> bin\physfs.def
+@echo "_PHYSFS_readUBE64" >> bin\physfs.def
+@echo "_PHYSFS_writeSLE16" >> bin\physfs.def
+@echo "_PHYSFS_writeULE16" >> bin\physfs.def
+@echo "_PHYSFS_writeSLE32" >> bin\physfs.def
+@echo "_PHYSFS_writeULE32" >> bin\physfs.def
+@echo "_PHYSFS_writeSLE64" >> bin\physfs.def
+@echo "_PHYSFS_writeULE64" >> bin\physfs.def
+@echo "_PHYSFS_writeSBE16" >> bin\physfs.def
+@echo "_PHYSFS_writeUBE16" >> bin\physfs.def
+@echo "_PHYSFS_writeSBE32" >> bin\physfs.def
+@echo "_PHYSFS_writeUBE32" >> bin\physfs.def
+@echo "_PHYSFS_writeSBE64" >> bin\physfs.def
+@echo "_PHYSFS_writeUBE64" >> bin\physfs.def
+@echo "_PHYSFS_setBuffer" >> bin\physfs.def
+@echo "_PHYSFS_flush" >> bin\physfs.def
+@echo "_PHYSFS_mount" >> bin\physfs.def
+@echo "_PHYSFS_getMountPoint" >> bin\physfs.def
+@echo "_PHYSFS_setAllocator" >> bin\physfs.def
+@echo "_PHYSFS_getCdRomDirsCallback" >> bin\physfs.def
+@echo "_PHYSFS_getSearchPathCallback" >> bin\physfs.def
+@echo "_PHYSFS_enumerateFilesCallback" >> bin\physfs.def
+@echo "_PHYSFS_utf8toucs2" >> bin\physfs.def
+@echo "_PHYSFS_utf8fromucs2" >> bin\physfs.def
+@echo "_PHYSFS_utf8toucs4" >> bin\physfs.def
+@echo "_PHYSFS_utf8fromucs4" >> bin\physfs.def
+@echo "_PHYSFS_utf8fromlatin1" >> bin\physfs.def
 
 @echo Building export library...
 emximp -o bin/physfs.lib bin/physfs.def
+emximp -o bin/physfs.a bin/physfs.def
 
 @echo Compiling PhysicsFS library...
 @echo on
@@ -128,6 +131,7 @@ gcc %CFLAGS% -o bin/os2.obj platform/os2.c
 gcc %CFLAGS% -o bin/dir.obj archivers/dir.c
 gcc %CFLAGS% -o bin/grp.obj archivers/grp.c
 gcc %CFLAGS% -o bin/wad.obj archivers/wad.c
+gcc %CFLAGS% -o bin/lzma.obj archivers/lzma.c
 gcc %CFLAGS% -o bin/zip.obj archivers/zip.c
 gcc %CFLAGS% -o bin/qpak.obj archivers/qpak.c
 gcc %CFLAGS% -o bin/hog.obj archivers/hog.c
@@ -144,18 +148,28 @@ gcc %CFLAGS% -o bin/inftrees.obj zlib123/inftrees.c
 gcc %CFLAGS% -o bin/trees.obj zlib123/trees.c
 gcc %CFLAGS% -o bin/uncompr.obj zlib123/uncompr.c
 gcc %CFLAGS% -o bin/zutil.obj zlib123/zutil.c
+gcc %CFLAGS% -o bin/7zBuffer.obj lzma/7zBuffer.c
+gcc %CFLAGS% -o bin/7zCrc.obj lzma/7zCrc.c
+gcc %CFLAGS% -o bin/7zDecode.obj lzma/7zDecode.c
+gcc %CFLAGS% -o bin/7zExtract.obj lzma/7zExtract.c
+gcc %CFLAGS% -o bin/7zHeader.obj lzma/7zHeader.c
+gcc %CFLAGS% -o bin/7zIn.obj lzma/7zIn.c
+gcc %CFLAGS% -o bin/7zItem.obj lzma/7zItem.c
+gcc %CFLAGS% -o bin/7zMethodID.obj lzma/7zMethodID.c
+gcc %CFLAGS% -o bin/LzmaDecode.obj lzma/LzmaDecode.c
+gcc %CFLAGS% -o bin/LzmaStateDecode.obj lzma/LzmaStateDecode.c
 @echo off
 
 :dolinking
 @echo Linking PhysicsFS library...
-gcc %DEBUGFLAGS% -Zdll -Zcrtdll -Zomf -Zmt -Zmtd -o bin/physfs.dll bin/*.obj bin/physfs.def
+gcc %DEBUGFLAGS% -Zdll -Zcrtdll -Zomf -o bin/physfs.dll bin/*.obj bin/physfs.def
 
 rem goto :builddone
 
 @echo Compiling test program...
 gcc %CFLAGS% -o bin/test_physfs.obj test/test_physfs.c
 @echo Linking test program...
-gcc %DEBUGFLAGS% -Zomf -Zcrtdll -Zmt -Zmtd -o bin/test_physfs.exe bin/test_physfs.obj bin/physfs.lib bin/test_physfs.def
+gcc %DEBUGFLAGS% -Zomf -Zcrtdll -o bin/test_physfs.exe bin/test_physfs.obj bin/physfs.lib bin/test_physfs.def
 
 :builddone
 
