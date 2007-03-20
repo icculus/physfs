7703202ee5da6d52779c97599b036f2e5ebc9787
Ryan C. Gordon <icculus@icculus.org>
2007-03-20 17:53:42 +0000

    Fixed a FIXME in the documentation comments.

--- physfs.h
+++ physfs.h
@@ -1948,18 +1948,18 @@ typedef struct
  * (This is for limited, hardcore use. If you don't immediately see a need
  *  for it, you can probably ignore this forever.)
  *
- *  !!! FIXME: don't promise C runtime malloc.
- *
- * By default, PhysicsFS will use ANSI C malloc/realloc/calloc/free calls
- *  to manage dynamic memory, but in some uncommon cases, the app might want
- *  more control over the library's memory management. This lets you redirect
- *  physfs to use your own allocation routines instead. You can only call this
- *  function before PHYSFS_init(); if the library is initialized, it'll
- *  reject your efforts to change the allocator mid-stream. You may call this
- *  function after PHYSFS_deinit() if you are willing to shutdown the library
- *  and restart it with a new allocator; this is a safe and supported
- *  operation. The allocator remains intact between deinit/init calls.
- *  If you want to return to the default allocator, pass a NULL in here.
+ * By default, PhysicsFS will use whatever is reasonable for a platform
+ *  to manage dynamic memory (usually ANSI C malloc/realloc/calloc/free, but
+ *  some platforms might use something else), but in some uncommon cases, the
+ *  app might want more control over the library's memory management. This
+ *  lets you redirect PhysicsFS to use your own allocation routines instead.
+ *  You can only call this function before PHYSFS_init(); if the library is
+ *  initialized, it'll reject your efforts to change the allocator mid-stream.
+ *  You may call this function after PHYSFS_deinit() if you are willing to
+ *  shut down the library and restart it with a new allocator; this is a safe
+ *  and supported operation. The allocator remains intact between deinit/init
+ *  calls. If you want to return to the platform's default allocator, pass a
+ *  NULL in here.
  *
  * If you aren't immediately sure what to do with this function, you can
  *  safely ignore it altogether.
