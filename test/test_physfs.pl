ebde523d90a28139b0877bcb3b94e162594dd7e6
Ryan C. Gordon <icculus@icculus.org>
2010-02-01 23:04:25 -0500

    Added simple test for perl bindings.

---
+++ test/test_physfs.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use physfs;
+
+print "testing PhysicsFS Perl bindings...\n";
+
+print "init...\n";
+physfs::init("$0") or die("physfs::init('$0'): ".physfs::getLastError()."\n");
+
+print "user dir: ", physfs::getUserDir(), "\n";
+print "base dir: ", physfs::getBaseDir(), "\n";
+
+print "deinit...\n";
+physfs::deinit();
+
+print "done!\n";
+exit 0;
+
