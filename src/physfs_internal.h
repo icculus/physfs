c3228127f3c602f6926d61175545897acacfe5c2
Ryan C. Gordon <icculus@icculus.org>
2011-10-06 03:24:53 -0400

    Patched to compile.

--- src/physfs_internal.h
+++ src/physfs_internal.h
@@ -877,7 +877,7 @@ char *__PHYSFS_convertToDependent(const char *prepend,
 #define PHYSFS_BIG_ENDIAN  4321
 
 #if  defined(__i386__) || defined(__ia64__) || \
-     defined(_M_IX86) || defined(_M_IA64) || defined(_M_X64) \
+     defined(_M_IX86) || defined(_M_IA64) || defined(_M_X64) || \
     (defined(__alpha__) || defined(__alpha)) || \
      defined(__arm__) || defined(ARM) || \
     (defined(__mips__) && defined(__MIPSEL__)) || \
