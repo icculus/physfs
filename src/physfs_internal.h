366221821c4627853d6aaa1ed584f3e0b229d93c
Ryan C. Gordon <icculus@icculus.org>
2012-03-12 01:33:16 -0400

    Fixed stack smashing in newly-aligned __PHYSFS_smallAlloc().

--- src/physfs_internal.h
+++ src/physfs_internal.h
@@ -80,8 +80,9 @@ extern "C" {
 void *__PHYSFS_initSmallAlloc(void *ptr, PHYSFS_uint64 len);
 
 #define __PHYSFS_smallAlloc(bytes) ( \
-    __PHYSFS_initSmallAlloc((((bytes) < __PHYSFS_SMALLALLOCTHRESHOLD) ? \
-                             alloca((size_t)((bytes)+1)) : NULL), (bytes)) \
+    __PHYSFS_initSmallAlloc( \
+        (((bytes) < __PHYSFS_SMALLALLOCTHRESHOLD) ? \
+            alloca((size_t)((bytes)+sizeof(void*))) : NULL), (bytes)) \
 )
 
 void __PHYSFS_smallFree(void *ptr);
