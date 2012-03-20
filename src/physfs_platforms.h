2486105f4f3f4d24f0daa905f5f3b9b872fd285f
Ryan C. Gordon <icculus@icculus.org>
2012-03-19 20:41:14 -0400

    Make physfs_platforms.h define with a 1, so "#if X" works.
    Added a few other pertinent platforms.

--- src/physfs_platforms.h
+++ src/physfs_platforms.h
@@ -13,42 +13,46 @@
  */
 
 #if (defined __HAIKU__)
-#  define PHYSFS_PLATFORM_HAIKU
-#  define PHYSFS_PLATFORM_BEOS
-#  define PHYSFS_PLATFORM_POSIX
+#  define PHYSFS_PLATFORM_HAIKU 1
+#  define PHYSFS_PLATFORM_BEOS 1
+#  define PHYSFS_PLATFORM_POSIX 1
 #elif ((defined __BEOS__) || (defined __beos__))
-#  define PHYSFS_PLATFORM_BEOS
-#  define PHYSFS_PLATFORM_POSIX
+#  define PHYSFS_PLATFORM_BEOS 1
+#  define PHYSFS_PLATFORM_POSIX 1
 #elif (defined _WIN32_WCE) || (defined _WIN64_WCE)
 #  error PocketPC support was dropped from PhysicsFS 2.1. Sorry.
 #elif (((defined _WIN32) || (defined _WIN64)) && (!defined __CYGWIN__))
-#  define PHYSFS_PLATFORM_WINDOWS
+#  define PHYSFS_PLATFORM_WINDOWS 1
 #elif (defined OS2)
 #  error OS/2 support was dropped from PhysicsFS 2.1. Sorry.
 #elif ((defined __MACH__) && (defined __APPLE__))
 /* To check if iphone or not, we need to include this file */
 # include <TargetConditionals.h> 
 # if ((TARGET_IPHONE_SIMULATOR) || (TARGET_OS_IPHONE))
-#    define PHYSFS_PLATFORM_UNIX
-#    define PHYSFS_PLATFORM_POSIX
-#    define PHYSFS_NO_CDROM_SUPPORT
+#    define PHYSFS_PLATFORM_UNIX 1
+#    define PHYSFS_PLATFORM_POSIX 1
+#    define PHYSFS_NO_CDROM_SUPPORT 1
 #  else
-#    define PHYSFS_PLATFORM_MACOSX
-#    define PHYSFS_PLATFORM_POSIX
+#    define PHYSFS_PLATFORM_MACOSX 1
+#    define PHYSFS_PLATFORM_POSIX 1
 #  endif
 #elif defined(macintosh)
 #  error Classic Mac OS support was dropped from PhysicsFS 2.0. Move to OS X.
 #elif defined(__linux)
-#  define PHYSFS_PLATFORM_LINUX
-#  define PHYSFS_PLATFORM_UNIX
-#  define PHYSFS_PLATFORM_POSIX
+#  define PHYSFS_PLATFORM_LINUX 1
+#  define PHYSFS_PLATFORM_UNIX 1
+#  define PHYSFS_PLATFORM_POSIX 1
 #elif defined(__sun) || defined(sun)
-#  define PHYSFS_PLATFORM_SOLARIS
-#  define PHYSFS_PLATFORM_UNIX
-#  define PHYSFS_PLATFORM_POSIX
+#  define PHYSFS_PLATFORM_SOLARIS 1
+#  define PHYSFS_PLATFORM_UNIX 1
+#  define PHYSFS_PLATFORM_POSIX 1
+#elif defined(BSD)
+#  define PHYSFS_PLATFORM_BSD 1
+#  define PHYSFS_PLATFORM_UNIX 1
+#  define PHYSFS_PLATFORM_POSIX 1
 #elif defined(unix)
-#  define PHYSFS_PLATFORM_UNIX
-#  define PHYSFS_PLATFORM_POSIX
+#  define PHYSFS_PLATFORM_UNIX 1
+#  define PHYSFS_PLATFORM_POSIX 1
 #else
 #  error Unknown platform.
 #endif
