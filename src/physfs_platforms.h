e13803c10181a79ea97edb320c2842b32046c234
Ryan C. Gordon <icculus@icculus.org>
2015-04-19 23:25:29 -0400

    Removed some tabs and an extra semicolon.

--- src/physfs_platforms.h
+++ src/physfs_platforms.h
@@ -22,9 +22,9 @@
 #elif (defined _WIN32_WCE) || (defined _WIN64_WCE)
 #  error PocketPC support was dropped from PhysicsFS 2.1. Sorry.
 #elif ((defined WINAPI_FAMILY) && WINAPI_FAMILY == WINAPI_FAMILY_APP)
-#	define PHYSFS_PLATFORM_WINRT 1
-#	define PHYSFS_NO_CDROM_SUPPORT 1
-#   define PHYSFS_PLATFORM_WINDOWS 1;
+#   define PHYSFS_PLATFORM_WINRT 1
+#   define PHYSFS_NO_CDROM_SUPPORT 1
+#   define PHYSFS_PLATFORM_WINDOWS 1
 #elif (((defined _WIN32) || (defined _WIN64)) && (!defined __CYGWIN__))
 #  define PHYSFS_PLATFORM_WINDOWS 1
 #elif (defined OS2)
