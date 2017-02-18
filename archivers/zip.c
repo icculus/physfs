747e817eb40006d454deef173d897f6c2f8d4427
Ryan C. Gordon <icculus@icculus.org>
2017-02-17 20:48:02 -0500

    Fix bug with copying z_stream objects around in zip archiver (thanks, Andrei!).
    zlib expects us to use inflateCopy() to move a z_stream, then inflateEnd()
    the original, and apparently fails in later versions if you just try to
    memcpy() it.
    
    This is only a bug in the stable-2.0 and stable-1.0 branches; we replaced
    zlib with miniz on the development branch, which doesn't have this
    requirement (or an inflateCopy() function at all!).

--- archivers/zip.c
+++ archivers/zip.c
@@ -327,7 +327,8 @@ static int ZIP_seek(fvoid *opaque, PHYSFS_uint64 offset)
                 return(0);
 
             inflateEnd(&finfo->stream);
-            memcpy(&finfo->stream, &str, sizeof (z_stream));
+            inflateCopy(&finfo->stream, &str);
+            inflateEnd(&str);
             finfo->uncompressed_position = finfo->compressed_position = 0;
         } /* if */
 
