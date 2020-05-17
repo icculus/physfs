db2a4a5807d6d3c468f0d68de738e0758b262f83
Ryan C. Gordon <icculus@icculus.org>
2020-05-17 00:58:55 -0400

    Move buildbot script changes to stable-3.0 branch.

--- extras/buildbot-emscripten.sh
+++ extras/buildbot-emscripten.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$SDKDIR" ]; then
-    SDKDIR="/emsdk_portable"
+    SDKDIR="/emsdk"
 fi
 
 ENVSCRIPT="$SDKDIR/emsdk_env.sh"
@@ -33,7 +33,7 @@ echo "Configuring..."
 emcmake cmake -G "Ninja" -DPHYSFS_BUILD_SHARED=False -DCMAKE_BUILD_TYPE=MinSizeRel .. || exit $?
 
 echo "Building..."
-emcmake cmake --build . --config MinSizeRel || exit $?
+emmake cmake --build . --config MinSizeRel || exit $?
 
 set -e
 rm -rf "$TARBALL" physfs-emscripten
